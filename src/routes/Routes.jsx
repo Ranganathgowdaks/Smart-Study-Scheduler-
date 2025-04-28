0a9778064fee92cfca3d074232e2da6ab3de7559
Ranganathgowdaks <gowdaranganath916@gmail.com>
2025-04-28 20:45:39 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 import Tasks from "../components/Tasks/Tasks";
 import AuthPage from "../components/AuthPage";
@@ -6,18 +6,18 @@ import Subjects from "../components/Subjects/Subjects";
 import FocusTimer from "../components/FocusTimer/FocusTimer";
 import Classess from "../components/Classess/Classess";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <div>Oops! Something went wrong. Page not found.</div>, // Add error element
-    children: [
-      { index: true, element: <Tasks /> },
-      { path: "auth", element: <AuthPage /> },
-      { path: "tasks", element: <Tasks /> },
-      { path: "subjects", element: <Subjects /> },
-      { path: "focus-timer", element: <FocusTimer /> },
-      { path: "classess", element: <Classess /> },
-    ],
-  },
-]);
+export const router = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Tasks />} />
+        <Route path="auth" element={<AuthPage />} />
+        <Route path="tasks" element={<Tasks />} />
+        <Route path="subjects" element={<Subjects />} />
+        <Route path="focus-timer" element={<FocusTimer />} />
+        <Route path="classess" element={<Classess />} />
+        <Route path="*" element={<div>Oops! Page not found.</div>} />
+      </Route>
+    </Routes>
+  </Router>
+);
